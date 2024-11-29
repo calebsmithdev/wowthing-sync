1fb01dc62f8c6bde0a8caa26035a148e4fc63ccb
Caleb <3040113+calebsmithdev@users.noreply.github.com>
2024-11-29 12:23:57 -0600

    Enforcing single instances

--- src-tauri/src/setup/desktop.rs
+++ src-tauri/src/setup/desktop.rs
@@ -1,4 +1,4 @@
-use tauri::AppHandle;
+use tauri::{AppHandle, Manager};
 use tauri_plugin_autostart::MacosLauncher;
 
 pub fn setup_desktop(app: &AppHandle) -> tauri::Result<()> {
@@ -7,6 +7,11 @@ pub fn setup_desktop(app: &AppHandle) -> tauri::Result<()> {
         Some(vec!["--flag1", "--flag2"]), // arbitrary number of args to pass to your app
     ))?;
     app.plugin(tauri_plugin_updater::Builder::new().build())?;
+    app.plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
+        let w = app.get_webview_window("main").unwrap();
+        w.show().unwrap();
+        w.set_focus().unwrap();
+    }));
 
     Ok(())
 }
--- src-tauri/src/setup/mac.rs
+++ src-tauri/src/setup/mac.rs
@@ -1,11 +1,8 @@
 use tauri::AppHandle;
 
 pub fn setup_mac(app: &AppHandle) -> tauri::Result<()> {
-    // Ignore if in dev mode for easier testing
-    if !cfg!(debug_assertions) {
-        // don't show on the taskbar/springboard
-        app.set_activation_policy(tauri::ActivationPolicy::Accessory)?;
-    }
+    // don't show on the taskbar/springboard
+    app.set_activation_policy(tauri::ActivationPolicy::Accessory)?;
 
     Ok(())
 }
